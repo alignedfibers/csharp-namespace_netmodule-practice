a400867965e021cbfa293b2baf2286f429adfbc6
starsnic@gmail.com <starsnic@gmail.com>
2017-01-23 02:56:47 -0600

    initial commit

---
+++ makeItWork.cs
@@ -0,0 +1,11 @@
+using System;
+using testNs;
+
+namespace testNsTwo{
+
+	class makeItWork{
+		public static void Main(){
+			test.dspHello();
+		}
+	}
+}
---
+++ testNs.cs
@@ -0,0 +1,14 @@
+//Namespace Test Shawn Stark - Fall 2009
+using System;
+namespace testNs{
+
+	class test{
+	
+		public test(){
+		}
+		public static void dsphello(){
+			Console.WriteLine("Hello");
+		}
+		
+	}
+}
